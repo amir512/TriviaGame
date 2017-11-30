d14d83cfc9903d191c0645443d1f38743e3557d8
Amir Ahmed <amir@Amirs-MacBook-Pro.local>
2017-11-29 22:05:23 -0600

    add logic to check users answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,35 @@ $("document").ready(function() {
 
 	//Write questions to the webpage
 	$("#questions").append("<h4>1. What is the capital of Texas?");
+	$("#questions").append("<input type='radio' name='capital' id='capital-lincoln' value='lincoln'>Lincoln</input>");
+	$("#questions").append(" <input type='radio' name='capital' id='capital-nyc' value='nyc'>New York City</input)");
+	$("#questions").append(" <input type='radio' name='capital' id='capital-denver' value='denver'>Denver</input)");
+	$("#questions").append(" <input type='radio' name='capital' id='capital-austin' value='austin'>Austin</input)");
 
+	
+	$("#questions").append("<br>");
+
+	//Add button for user to submit answers
+	$("#questions").append("<button id='submit'>Submit</button>");
+
+
+	//Check answers when user clicks submit
+	$("#submit").on("click", function() {
+
+		if ($("input:radio[id='capital-austin']").is(":checked")) {
+		correct++;
+		//alert(correct);
+		//$("#results").text(correct);
+       
+	};
+
+	$("#results").text("Questions answered correctly: " + correct);
+
+
+
+	})
+
+	
 
 
 
