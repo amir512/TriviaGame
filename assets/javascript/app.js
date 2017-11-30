268e741d5eaf9d33699f1e3c538db8bd56a68139
Amir Ahmed <amir@Amirs-MacBook-Pro.local>
2017-11-29 21:28:07 -0600

    added html css and JS files

---
+++ assets/javascript/app.js
@@ -0,0 +1,38 @@
+$("document").ready(function() {
+
+	//Establish variables to keep track of number of questions answered correct/wrong
+	var correct = 0;
+	var wrong = 0;
+
+	//Write questions to the webpage
+	$("#questions").append("<h4>1. What is the capital of Texas?");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
